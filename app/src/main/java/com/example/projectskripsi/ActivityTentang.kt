89cf2263c8ab742a2467c4b9232da8a82df80e4f
HelBad <helbad3@gmail.com>
2022-01-24 21:53:19 +0700

    UI About

--- app/src/main/java/com/example/projectskripsi/ActivityTentang.kt
+++ app/src/main/java/com/example/projectskripsi/ActivityTentang.kt
@@ -2,11 +2,35 @@ package com.example.projectskripsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.viewpager2.widget.ViewPager2
+import com.example.projectskripsi.adapter.SliderAdapter
+import com.example.projectskripsi.adapter.SliderItem
 
 class ActivityTentang : AppCompatActivity() {
+    lateinit var viewPager2: ViewPager2
+    private val sliderHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tentang)
+
+        viewPager2 = findViewById(R.id.gambarTentang)
+        val sliderItems: MutableList<SliderItem> = ArrayList()
+        sliderItems.add(SliderItem(R.drawable.sampel))
+        sliderItems.add(SliderItem(R.drawable.sampel))
+        sliderItems.add(SliderItem(R.drawable.sampel))
+        viewPager2.adapter = SliderAdapter(sliderItems, viewPager2)
+        viewPager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                sliderHandler.removeCallbacks(sliderRunnable)
+                sliderHandler.postDelayed(sliderRunnable, 3000)
+            }
+        })
+    }
+
+    private val sliderRunnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
     }
 }
--- app/src/main/java/com/example/projectskripsi/fragment/FragmentBeranda.kt
+++ app/src/main/java/com/example/projectskripsi/fragment/FragmentBeranda.kt
@@ -2,26 +2,28 @@ package com.example.projectskripsi.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.*
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
+import com.example.projectskripsi.ActivityCheckout
 import com.example.projectskripsi.ActivityDetail
+import com.example.projectskripsi.ActivityTentang
 import com.example.projectskripsi.R
-import com.example.projectskripsi.adapter.SliderAdapter
-import com.example.projectskripsi.adapter.SliderItem
 import com.example.projectskripsi.model.Menu
 import com.example.projectskripsi.adapter.ViewholderBeranda
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.google.firebase.database.FirebaseDatabase
 
 class FragmentBeranda : Fragment() {
-    lateinit var viewPager2: ViewPager2
-    private val sliderHandler = Handler()
     lateinit var mLayoutManager: LinearLayoutManager
     lateinit var mRecyclerView: RecyclerView
+    lateinit var keranjangBeranda: ImageView
+    lateinit var spinnerBeranda: Spinner
+    lateinit var penyakitBeranda: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_beranda, container, false)
@@ -30,28 +32,51 @@ class FragmentBeranda : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewPager2 = requireActivity().findViewById(R.id.gambarUtama)
-        val sliderItems: MutableList<SliderItem> = ArrayList()
-        sliderItems.add(SliderItem(R.drawable.sampel))
-        sliderItems.add(SliderItem(R.drawable.sampel))
-        sliderItems.add(SliderItem(R.drawable.sampel))
-        viewPager2.adapter = SliderAdapter(sliderItems, viewPager2)
-        viewPager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                sliderHandler.removeCallbacks(sliderRunnable)
-                sliderHandler.postDelayed(sliderRunnable, 3000)
-            }
-        })
+        setHasOptionsMenu(true)
+        val actionBar = requireActivity().findViewById(R.id.toolbarBeranda) as Toolbar
+        (activity as AppCompatActivity).setSupportActionBar(actionBar)
+
+        keranjangBeranda = requireActivity().findViewById(R.id.keranjangBeranda)
+        spinnerBeranda = requireActivity().findViewById(R.id.spinnerBeranda)
+        penyakitBeranda = requireActivity().findViewById(R.id.penyakitBeranda)
 
         mLayoutManager = LinearLayoutManager(requireActivity())
         mRecyclerView = requireView().findViewById(R.id.recyclerBeranda)
         mRecyclerView.setHasFixedSize(true)
         mRecyclerView.layoutManager = mLayoutManager
+
+        val genderUser = arrayOf("Sehat", "Obesitas", "Diabetes", "Asam Lambung")
+        spinnerBeranda.adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1, genderUser)
+        spinnerBeranda.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                penyakitBeranda.text = "Baik Untuk Penyakit"
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                penyakitBeranda.text = genderUser[position]
+            }
+        }
+
+        keranjangBeranda.setOnClickListener {
+            val intent = Intent(view.context, ActivityCheckout::class.java)
+            startActivity(intent)
+        }
     }
 
-    private val sliderRunnable = Runnable {
-        viewPager2.currentItem = viewPager2.currentItem + 1
+    override fun onCreateOptionsMenu(menu: android.view.Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        val inflater = requireActivity().menuInflater
+        inflater.inflate(R.menu.bar_beranda, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.about) {
+            val intent = Intent(activity, ActivityTentang::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onStart() {
