b388836517c113c0e584cf0a754794c678648c29
HelBad <helbad3@gmail.com>
2022-02-04 00:04:43 +0700

    Admin Edit Menu

--- app/src/main/java/com/example/projectskripsi/admin/ActivityDetail.kt
+++ app/src/main/java/com/example/projectskripsi/admin/ActivityDetail.kt
@@ -1,14 +1,17 @@
 package com.example.projectskripsi.admin
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.example.projectskripsi.R
 import com.example.projectskripsi.model.Menu
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import java.text.DecimalFormat
 import java.text.NumberFormat
@@ -25,6 +28,7 @@ class ActivityDetail : AppCompatActivity() {
     lateinit var btnEdit: Button
     lateinit var btnHapus: Button
 
+    lateinit var alertDialog: AlertDialog.Builder
     var formatNumber: NumberFormat = DecimalFormat("#,###")
     var id_menu = ""
 
@@ -43,6 +47,7 @@ class ActivityDetail : AppCompatActivity() {
         btnEdit = findViewById(R.id.btnEdit)
         btnHapus = findViewById(R.id.btnHapus)
 
+        alertDialog = AlertDialog.Builder(this)
         loadData()
     }
 
@@ -65,11 +70,26 @@ class ActivityDetail : AppCompatActivity() {
 
                     btnEdit.setOnClickListener {
                         val intent = Intent(this@ActivityDetail, ActivityEdit::class.java)
+                        intent.putExtra("id_menu", id_menu)
                         startActivity(intent)
                     }
                     btnHapus.setOnClickListener {
-//                        FirebaseDatabase.getInstance().getReference("menu")
-//                            .child(intent.getStringExtra("id_menu").toString()).removeValue()
+                        alertDialog.setMessage("Apakah anda ingin menghapus menu ini ?")
+                            .setCancelable(false)
+                            .setPositiveButton("YA", object: DialogInterface.OnClickListener {
+                                override fun onClick(dialog: DialogInterface, id:Int) {
+                                    FirebaseDatabase.getInstance().getReference("menu")
+                                        .child(intent.getStringExtra("id_menu").toString()).removeValue()
+                                    FirebaseStorage.getInstance().getReference("menu")
+                                        .child(intent.getStringExtra("id_menu").toString()).delete()
+                                    finish()
+                                }
+                            })
+                            .setNegativeButton("TIDAK", object: DialogInterface.OnClickListener {
+                                override fun onClick(dialog: DialogInterface, id:Int) {
+                                    dialog.cancel()
+                                }
+                            }).create().show()
                     }
                 }
             }
--- app/src/main/java/com/example/projectskripsi/admin/ActivityEdit.kt
+++ app/src/main/java/com/example/projectskripsi/admin/ActivityEdit.kt
@@ -1,12 +1,184 @@
 package com.example.projectskripsi.admin
 
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.projectskripsi.R
+import com.example.projectskripsi.model.Menu
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class ActivityEdit : AppCompatActivity() {
+    lateinit var namaMenu: EditText
+    lateinit var deskripsiMenu: EditText
+    lateinit var hargaMenu: EditText
+    lateinit var gambarMenu: TextView
+    lateinit var mineralMenu: EditText
+    lateinit var proteinMenu: EditText
+    lateinit var lemakMenu: EditText
+    lateinit var karbohidratMenu: EditText
+    lateinit var btnSimpan: Button
+
+    lateinit var alertDialog: AlertDialog.Builder
+    lateinit var databaseReference: DatabaseReference
+    lateinit var storageReference: StorageReference
+    lateinit var uri: Uri
+    var url: Uri? = null
+    var statusMenu = ""
+    var id_menu = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.admin_activity_edit)
+
+        namaMenu = findViewById(R.id.namaMenu)
+        deskripsiMenu = findViewById(R.id.deskripsiMenu)
+        hargaMenu = findViewById(R.id.hargaMenu)
+        gambarMenu = findViewById(R.id.gambarMenu)
+        mineralMenu = findViewById(R.id.mineralMenu)
+        proteinMenu = findViewById(R.id.proteinMenu)
+        lemakMenu = findViewById(R.id.lemakMenu)
+        karbohidratMenu = findViewById(R.id.karbohidratMenu)
+        btnSimpan = findViewById(R.id.btnSimpan)
+
+        alertDialog = AlertDialog.Builder(this)
+        databaseReference = FirebaseDatabase.getInstance().getReference("menu")
+        storageReference = FirebaseStorage.getInstance().getReference("menu")
+        loadMenu()
+
+        gambarMenu.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+        btnSimpan.setOnClickListener {
+            alertDialog.setMessage("Apakah anda ingin menyimpan data menu ?")
+                .setCancelable(false)
+                .setPositiveButton("YA", object: DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface, id:Int) {
+                        if(validate()) {
+                            tambahData()
+                            val intent = Intent(this@ActivityEdit, ActivityUtama::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                })
+                .setNegativeButton("TIDAK", object: DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface, id:Int) {
+                        dialog.cancel()
+                    }
+                }).create().show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK) {
+            if(requestCode == 0) {
+                if(statusMenu == "ada") {
+                    id_menu = intent.getStringExtra("id_menu").toString()
+                } else {
+                    id_menu = databaseReference.push().key.toString()
+                }
+                uri = data!!.data!!
+                var mStorage = storageReference.child(id_menu)
+                try {
+                    mStorage.putFile(uri).addOnFailureListener {}.addOnSuccessListener {
+                        mStorage.downloadUrl.addOnCompleteListener { taskSnapshot ->
+                            url = taskSnapshot.result
+                            gambarMenu.text = url.toString()
+                        }}
+                } catch(ex:Exception) {
+                    Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun loadMenu() {
+        databaseReference.orderByChild("id_menu").equalTo(intent.getStringExtra("id_menu"))
+            .addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(datasnapshot: DataSnapshot) {
+                    for (snapshot1 in datasnapshot.children) {
+                        val allocation = snapshot1.getValue(Menu::class.java)
+                        namaMenu.text = Editable.Factory.getInstance().newEditable(allocation!!.nama_menu)
+                        deskripsiMenu.text = Editable.Factory.getInstance().newEditable(allocation.deskripsi)
+                        mineralMenu.text = Editable.Factory.getInstance().newEditable(allocation.mineral)
+                        proteinMenu.text = Editable.Factory.getInstance().newEditable(allocation.protein)
+                        lemakMenu.text = Editable.Factory.getInstance().newEditable(allocation.lemak)
+                        karbohidratMenu.text = Editable.Factory.getInstance().newEditable(allocation.karbohidrat)
+                        hargaMenu.text = Editable.Factory.getInstance().newEditable(allocation.harga)
+                        gambarMenu.text = allocation.gambar
+                        statusMenu = "ada"
+                    }
+                }
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+    }
+
+    private fun validate(): Boolean {
+        if(namaMenu.text.toString() == "") {
+            Toast.makeText(this, "Nama menu masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(deskripsiMenu.text.toString() == "") {
+            Toast.makeText(this, "Deskripsi masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(hargaMenu.text.toString() == "") {
+            Toast.makeText(this, "Harga masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(gambarMenu.text.toString() == "") {
+            Toast.makeText(this, "Gambar masih kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(mineralMenu.text.toString() == "") {
+            Toast.makeText(this, "Jumlah mineral kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(proteinMenu.text.toString() == "") {
+            Toast.makeText(this, "Jumlah protein kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(lemakMenu.text.toString() == "") {
+            Toast.makeText(this, "Jumlah lemak kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(karbohidratMenu.text.toString() == "") {
+            Toast.makeText(this, "Jumlah karbohidrat kosong", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun tambahData() {
+        if(statusMenu == "ada") {
+            id_menu = intent.getStringExtra("id_menu").toString()
+            databaseReference.child(id_menu).child("nama_menu").setValue(namaMenu.text.toString())
+            databaseReference.child(id_menu).child("deskripsi").setValue(deskripsiMenu.text.toString())
+            databaseReference.child(id_menu).child("mineral").setValue(mineralMenu.text.toString())
+            databaseReference.child(id_menu).child("protein").setValue(proteinMenu.text.toString())
+            databaseReference.child(id_menu).child("lemak").setValue(lemakMenu.text.toString())
+            databaseReference.child(id_menu).child("karbohidrat").setValue(karbohidratMenu.text.toString())
+            databaseReference.child(id_menu).child("harga").setValue(hargaMenu.text.toString())
+            databaseReference.child(id_menu).child("gambar").setValue(gambarMenu.text.toString())
+        } else {
+            val addData = Menu(id_menu, namaMenu.text.toString(), deskripsiMenu.text.toString(),
+                mineralMenu.text.toString(), proteinMenu.text.toString(), lemakMenu.text.toString(),
+                karbohidratMenu.text.toString(), hargaMenu.text.toString(), gambarMenu.text.toString())
+            databaseReference.child(id_menu).setValue(addData)
+        }
     }
 }
